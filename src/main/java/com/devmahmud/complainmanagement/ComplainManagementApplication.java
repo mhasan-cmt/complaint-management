808e4f9e26a3e9e00c258a9a02d015a608cf1e8d
Mahmudul Hasan <mhshafin220@gmail.com>
2022-06-13 22:56:43 +0600

    initial commit

---
+++ src/main/java/com/devmahmud/complainmanagement/ComplainManagementApplication.java
@@ -0,0 +1,13 @@
+package com.devmahmud.complainmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplainManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmahmud/complainmanagement/ComplainManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmahmud.complainmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplainManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
